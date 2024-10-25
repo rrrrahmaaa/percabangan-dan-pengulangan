37bcf3f57a934b7f4dee22e8060af225c63b8a59
rrrrahmaaa <161696394+rrrrahmaaa@users.noreply.github.com>
2024-10-25 12:38:09 +0700

    Create shape.js

---
+++ shape.js
@@ -0,0 +1,31 @@
+const segitiga = {
+    luas: (alas, tinggi) => {
+      return (alas * tinggi) / 2;
+    },
+    keliling: (sisiA, sisiB, sisiC) => {
+      return sisiA + sisiB + sisiC;
+    },
+  };
+  
+  const persegi = {
+    luas: (sisi) => {
+      return sisi * sisi;
+    },
+    keliling: (sisi) => {
+      return 4 * sisi;
+    },
+  };
+  
+  const persegiPanjang = {
+    luas: (panjang, lebar) => {
+      return panjang * lebar;
+    },
+    keliling: (panjang, lebar) => {
+      return 2 * (panjang + lebar);
+    },
+  };
+  module.exports = {
+    segitiga,
+    persegi,
+    persegiPanjang,
+  };
